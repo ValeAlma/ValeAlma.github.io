14a499e2842752778779e1bd4906c37742d268f1
ValeAlma <126628711+ValeAlma@users.noreply.github.com>
2023-03-01 05:14:24 -0300

    Update eventos.js

--- js1/eventos.js
+++ js1/eventos.js
@@ -1,6 +1,6 @@
 function enAmarillo3() {
     Element =document.getElementById("perf");
-    Element.src="img1/pesonal2.png";
+    Element.src="img1/personal2.png";
     }
     function enNegro3() {
         Element =document.getElementById("perf");
